7fff2fee7ab286a61cc9842fb1c7d2fda47cb6f3
Vinothkumar Krishnamoorthy <vinothkumar.k@yodlee.com>
2020-07-21 15:53:27 +0530

    Adding node server

---
+++ index.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({
+	path: './config/config.env',
+});
+
+const PORT = process.env.PORT || 5000;
+const log = console.log;
+
+const app = express();
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+} else {
+	app.use(express.static('client/public'));
+
+	app.get('*', (req, res) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'))
+	);
+}
+
+app.listen(PORT, () => {
+	log(`Server started in ${process.env.NODE_ENV} mode at port ${PORT}`);
+});
