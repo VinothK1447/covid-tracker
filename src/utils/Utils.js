1f70ad68f7d815bf294fbcd96eda81623949d78a
VinothK1447 <33567515+VinothK1447@users.noreply.github.com>
2021-05-19 11:42:34 +0530

    Null checks added

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,13 +1,13 @@
-import { indiaStateCodeMap } from './StateCodeMap';
-import strings from '../config/strings';
+import { indiaStateCodeMap } from './StateCodeMap'
+import strings from '../config/strings'
 class Utils {
 	static formatNumber(number) {
-		let locale = navigator.language;
-		return new Intl.NumberFormat(locale).format(number);
+		let locale = navigator.language
+		return new Intl.NumberFormat(locale).format(number)
 	}
 
 	static formatDate(date) {
-		let locale = navigator.language;
+		let locale = navigator.language
 		let options = {
 			year: 'numeric',
 			month: 'numeric',
@@ -15,51 +15,39 @@ class Utils {
 			hour: 'numeric',
 			minute: 'numeric',
 			second: 'numeric',
-			hour12: true,
-		};
-		return new Intl.DateTimeFormat(locale, options).format(date);
+			hour12: true
+		}
+		return new Intl.DateTimeFormat(locale, options).format(date)
 	}
 
 	static dateTimeDiff(lastUpdated) {
-		let diff = Math.floor(
-			new Date().getTime() - new Date(lastUpdated).getTime()
-		);
-		let day = 1000 * 60 * 60 * 24;
+		let diff = Math.floor(new Date().getTime() - new Date(lastUpdated).getTime())
+		let day = 1000 * 60 * 60 * 24
 
-		let days = Math.floor(diff / day);
-		let months = Math.floor(days / 31);
-		let years = Math.floor(months / 12);
+		let days = Math.floor(diff / day)
+		let months = Math.floor(days / 31)
+		let years = Math.floor(months / 12)
 
-		let remHrs = diff % 86400000;
-		let remMin = remHrs % 3600000;
+		let remHrs = diff % 86400000
+		let remMin = remHrs % 3600000
 
-		let hours = Math.floor(remHrs / 3600000);
-		let minutes = Math.floor(remMin / 60000);
+		let hours = Math.floor(remHrs / 3600000)
+		let minutes = Math.floor(remMin / 60000)
 
 		if (years > 0) {
-			return ` (${strings.en.about} ${years} ${strings.en.year}${
-				years > 1 ? strings.en.timeSuffix : ''
-			}  ${strings.en.ago})`;
+			return ` (${strings.en.about} ${years} ${strings.en.year}${years > 1 ? strings.en.timeSuffix : ''}  ${strings.en.ago})`
 		}
 		if (months > 0) {
-			return ` (${strings.en.about} ${months} ${strings.en.month}${
-				months > 1 ? strings.en.timeSuffix : ''
-			} ${strings.en.ago})`;
+			return ` (${strings.en.about} ${months} ${strings.en.month}${months > 1 ? strings.en.timeSuffix : ''} ${strings.en.ago})`
 		}
 		if (days > 0) {
-			return ` (${strings.en.about} ${days} ${strings.en.day}${
-				days > 1 ? strings.en.timeSuffix : ''
-			} ${strings.en.ago})`;
+			return ` (${strings.en.about} ${days} ${strings.en.day}${days > 1 ? strings.en.timeSuffix : ''} ${strings.en.ago})`
 		}
 		if (hours > 0) {
-			return ` (${strings.en.about} ${hours} ${strings.en.hour}${
-				hours > 1 ? strings.en.timeSuffix : ''
-			} ${strings.en.ago})`;
+			return ` (${strings.en.about} ${hours} ${strings.en.hour}${hours > 1 ? strings.en.timeSuffix : ''} ${strings.en.ago})`
 		}
 		if (minutes > 0) {
-			return ` (${strings.en.about} ${minutes} ${strings.en.minute}${
-				minutes > 1 ? strings.en.timeSuffix : ''
-			} ${strings.en.ago})`;
+			return ` (${strings.en.about} ${minutes} ${strings.en.minute}${minutes > 1 ? strings.en.timeSuffix : ''} ${strings.en.ago})`
 		}
 	}
 
@@ -67,86 +55,63 @@ class Utils {
 		let allCountries = allCountriesData.map((country) => {
 			return {
 				country: country.country,
-				countryInfo: country.countryInfo,
-			};
-		});
+				countryInfo: country.countryInfo
+			}
+		})
 
-		return allCountries;
+		return allCountries
 	}
 
 	static getSelectedCountryInfo(allCountriesData, selectedCountry) {
-		let selectedCountryInfo = allCountriesData.filter(
-			(country) => country.country === selectedCountry
-		);
-		return selectedCountryInfo;
+		let selectedCountryInfo = allCountriesData.filter((country) => country.country === selectedCountry)
+		return selectedCountryInfo
 	}
 
 	static appendNormalizedBasicInfo(data) {
 		Object.keys(data).map((key) => {
 			if (indiaStateCodeMap.hasOwnProperty(key)) {
-				data[key] = Object.assign(data[key], indiaStateCodeMap[key]);
+				data[key] = Object.assign(data[key], indiaStateCodeMap[key])
 				if (data[key].total) {
-					let totals = data[key].total;
-					let {
-						recovered = 0,
-						confirmed = 0,
-						migrated = 0,
-						deceased = 0,
-					} = totals;
-					let active = confirmed - (recovered + migrated + deceased);
+					let totals = data[key].total
+					let { recovered = 0, confirmed = 0, migrated = 0, deceased = 0 } = totals
+					let active = confirmed - (recovered + migrated + deceased)
 					data[key].total = Object.assign(data[key].total, {
-						active: active,
-					});
+						active: active
+					})
 				}
 				if (data[key].meta && data[key].meta.notes) {
-					data[key].meta.notes = data[key].meta.notes.replace(
-						/\n/g,
-						'<br />'
-					);
+					data[key].meta.notes = data[key].meta.notes.replace(/\n/g, '<br />')
 				}
 				if (data[key].districts) {
 					Object.keys(data[key].districts).map((district) => {
-						let {
-							recovered = 0,
-							confirmed = 0,
-							migrated = 0,
-							deceased = 0,
-						} = data[key].districts[district].total;
-						let active =
-							confirmed - (recovered + migrated + deceased);
-						data[key].districts[district].total = {
-							...data[key].districts[district].total,
-							active,
-						};
-						return false;
-					});
+						if (data[key].districts[district].total) {
+							let { recovered = 0, confirmed = 0, migrated = 0, deceased = 0 } = data[key].districts[district].total
+							let active = confirmed - (recovered + migrated + deceased)
+							data[key].districts[district].total = {
+								...data[key].districts[district].total,
+								active
+							}
+						}
+						return false
+					})
 				}
 			}
-			return false;
-		});
-		return data;
+			return false
+		})
+		return data
 	}
 
 	static normalizeVaccineData(data) {
 		if (data) {
 			data = data.map((item, idx) => {
-				item.details = item.details.replace(
-					/Outcomes:/g,
-					'<br/>Outcomes: '
-				);
-				item.details = item.details.replace(
-					/Status:/g,
-					'<br/>Status: '
-				);
-				item.details = item.details.replace(
-					/Study Design:/g,
-					'<br/>Study Design: '
-				);
-				return item;
-			});
+				item.details = item.details.replace(/Outcomes:/g, '<br/>Outcomes: ')
+				item.details = item.details.replace(/Status:/g, '<br/>Status: ')
+				item.details = item.details.replace(/Study Design:/g, '<br/>Study Design: ')
+				return item
+			})
 		}
-		return data;
+		return data
 	}
 }
 
-export default Utils;
+export default Utils
